dfecd48df4ebed28fd02c12034d59ae0080f0fd2
md Yasin <mdyasin48902@gmail.com>
2025-02-04 22:24:44 +0600

    jacket section added with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-       
+        screens: {
+          'sm-max': { 'max': '639px' },
+          'md-max': { 'max': '767px' },  
+          'lg-max': { 'max': '1023px' }, 
+          'xl-max': { 'max': '1279px' }, 
+        },
       },
     },
     plugins: [],
